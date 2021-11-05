d4b446cb42974c6d306bc7b025085f31d3b5cc84
Alexandre Picinato Scherer <83470126+ElMaxh0@users.noreply.github.com>
2021-11-05 10:46:58 -0300

    Add Two Images
    Adicionada Duas Imagens

--- imersivesource.js
+++ imersivesource.js
@@ -1,10 +1,14 @@
 //Adiciona as Propriedades Necessarias 
-var imgurl=new URLSearchParams(window.location.search).get('imgurl')
+//var imgurl=new URLSearchParams(window.location.search).get('imgurl')
+var imgurl="https://images3.alphacoders.com/744/thumb-1920-744829.jpg"
+var imgurl2="https://images3.alphacoders.com/866/866202.jpg"
 var javascript = document.createElement('script');
 var css = '.data{background: url("'+imgurl+'");}'
+var cssData = '.data2{background: url("'+imgurl2+'");}'
 style = document.createElement('style');
 javascript.src = './source.js';
 document.head.appendChild(javascript); 
 head = document.head || document.getElementsByTagName('head')[0],
 head.appendChild(style); 
 style.appendChild(document.createTextNode(css));
+style.appendChild(document.createTextNode(cssData));
